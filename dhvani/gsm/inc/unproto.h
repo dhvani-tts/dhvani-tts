24a18baa08b1aaa32b8831eb0f3e8254f7e41874
oazim <oazim@67f3dd4c-01a5-42c5-b7d6-d5ec67a24d75>
2001-09-12 18:25:07 +0000

    This commit was generated by cvs2svn to compensate for changes in r3, which included commits to RCS files with non-trunk default branches.
    git-svn-id: http://svn.code.sf.net/p/dhvani/code/trunk@4 67f3dd4c-01a5-42c5-b7d6-d5ec67a24d75

---
+++ dhvani/gsm/inc/unproto.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 1992 by Jutta Degener and Carsten Bormann, Technische
+ * Universitaet Berlin.  See the accompanying file "COPYRIGHT" for
+ * details.  THERE IS ABSOLUTELY NO WARRANTY FOR THIS SOFTWARE.
+ */
+
+/*$Header: /home/santhosh/dhvani-cvsbackup/dhvani/gsm/inc/unproto.h,v 1.1.1.1 2001-09-12 18:24:59 oazim Exp $*/
+
+#ifdef	PROTO_H		/* sic */
+#undef	PROTO_H
+
+#undef	P
+#undef	P0
+#undef	P1
+#undef	P2
+#undef	P3
+#undef	P4
+#undef	P5
+#undef	P6
+#undef	P7
+#undef	P8
+
+#endif	/* PROTO_H */
---
+++ dhvani/gsm/src/gsm_destroy.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright 1992 by Jutta Degener and Carsten Bormann, Technische
+ * Universitaet Berlin.  See the accompanying file "COPYRIGHT" for
+ * details.  THERE IS ABSOLUTELY NO WARRANTY FOR THIS SOFTWARE.
+ */
+
+/* $Header: /home/santhosh/dhvani-cvsbackup/dhvani/gsm/src/gsm_destroy.c,v 1.1.1.1 2001-09-12 18:24:59 oazim Exp $ */
+
+#include "gsm.h"
+#include "config.h"
+#include "proto.h"
+
+#ifdef	HAS_STDLIB_H
+#	include	<stdlib.h>
+#else
+#	ifdef	HAS_MALLOC_H
+#		include 	<malloc.h>
+#	else
+		extern void free();
+#	endif
+#endif
+
+void gsm_destroy P1((S), gsm S)
+{
+	if (S) free((char *)S);
+}
