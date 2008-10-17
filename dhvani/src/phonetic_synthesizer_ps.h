cbdc939c18452e1d183f9c70dcc4025a6c82ef08
santhosh_tr <santhosh_tr@67f3dd4c-01a5-42c5-b7d6-d5ec67a24d75>
2008-10-17 17:06:08 +0000

    updated for 0.94
    git-svn-id: http://svn.code.sf.net/p/dhvani/code/trunk@39 67f3dd4c-01a5-42c5-b7d6-d5ec67a24d75

---
+++ dhvani/src/phonetic_synthesizer_ps.h
@@ -0,0 +1,28 @@
+/* phonetic_synthesizer_ps.h
+ *
+ * Copyright (C) 2008
+ *  Zabeeh Khan <zabeehkhan@gmail.com>, The Pathanisation Project.
+ *  http://pathanisation.pakhtosoft.com	
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef PHONETIC_SYNTHESIZER_PS_H
+#define PHONETIC_SYNTHESIZER_PS_H
+#include <stdio.h>
+#include <stdlib.h>
+#define no_of_halfs 61
+char *generate_phonetic_script_ps(unsigned short *, int);
+#endif
