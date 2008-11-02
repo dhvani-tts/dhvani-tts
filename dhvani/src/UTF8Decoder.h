3c769bcdd2f0344b12517f03fbc64fddecd96a60
santhosh_tr <santhosh_tr@67f3dd4c-01a5-42c5-b7d6-d5ec67a24d75>
2008-11-02 10:35:27 +0000

    Adding new files
    git-svn-id: http://svn.code.sf.net/p/dhvani/code/trunk@47 67f3dd4c-01a5-42c5-b7d6-d5ec67a24d75

---
+++ dhvani/src/UTF8Decoder.h
@@ -0,0 +1,32 @@
+/* UTF8Decoder.h
+ *
+ * Copyright (C) 2007-2008
+ *  Santhosh Thottingal <santhosh.thottingal@gmail.com>, 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef UTF8DECODER_H_
+#define UTF8DECODER_H_
+#include <stdio.h>
+
+/* structure to hold unicode input */
+struct code {
+    int type;
+    unsigned short alpha;
+    unsigned char beta;
+};
+struct code utf8_to_utf16_text(unsigned char *, int *);
+struct code utf8_to_utf16_file(FILE *);
+#endif /*UTF8DECODER_H_ */
