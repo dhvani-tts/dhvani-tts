760539f769be66610f329534d16282752a0932f8
Prasanna Kumar T S M <prasannatsmkumar@gmail.com>
2013-10-05 10:27:57 +0530

    Fixed memory leaks reported by cppcheck.

--- src/phonetic_synthesizer_or.c
+++ src/phonetic_synthesizer_or.c
@@ -693,14 +693,15 @@ char *or_spellNumbers(short *word, int start, int end)
 	char *final = (char *)malloc(100 * sizeof(char *));
 	final[0] = '\0';
 	char *decimal = (char *)malloc(100 * sizeof(char *));
-	for (i = start; i < end; i++) {
 
+	for (i = start; i < end; i++) {
 		decimal[0] = word[i];
 		decimal[1] = '\0';
 		final = strcat(final, or_parsenum(or_replacenum(decimal, 1)));
 		final = strcat(final, " G1500 ");
-
 	}
+
+	free(decimal); decimal = NULL;
 
 	return final;
 }
@@ -933,6 +934,10 @@ char *or_parseword(int last)
 		}
 		i -= dcr;
 	}
+
+	free(lsyl); lsyl = NULL;
+	free(t_half); t_half = NULL;
+
 	return (syllable);
 }
 
